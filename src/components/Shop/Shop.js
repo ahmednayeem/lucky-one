f4e56f7edee08e23aec13e838b109e0231d00e47
ahmednayeeme <ahmednaim353@gmail.com>
2022-03-26 01:43:04 +0600

    header-fixed

---
+++ src/components/Shop/Shop.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import './Shop.css';
+
+const Shop = () => {
+    return (
+        <div className='d-flex justify-content-center'>
+           <div className='col-md-8'>
+               <h1> Mobile Shop </h1>
+           </div>
+           <div className='col-md-4'>
+               <h5>Order Summary</h5>
+           </div>
+        </div>
+    );
+};
+
+export default Shop;
